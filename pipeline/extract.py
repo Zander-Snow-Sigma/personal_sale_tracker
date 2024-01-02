5e1e7c03ce32009b4b9ff1be1b2c53a9fe6c317f
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2024-01-02 14:24:52 +0000

    updated extract script to include domain name

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -3,10 +3,18 @@ Extracts/scrapes the desired information from the required product.
 """
 import json
 from os import environ
+from urllib.parse import urlparse
+
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import requests
+
+
+def get_domain_name(url: str) -> str:
+    parsed_url = urlparse(url)
+    domain_name = '{uri.netloc}'.format(uri=parsed_url)
+    return domain_name
 
 
 def scrape_asos_page(url: str, header: dict) -> dict:
@@ -17,17 +25,20 @@ def scrape_asos_page(url: str, header: dict) -> dict:
     soup = BeautifulSoup(page.text, "html.parser").find(
         "script", type="application/ld+json")
     product_data = json.loads(soup.string)
-    print(product_data)
 
     price_endpoint = f"https://www.asos.com/api/product/catalogue/v3/stockprice?productIds={product_data['productID']}&store=COM&currency=GBP"
 
     price = requests.get(price_endpoint).json()[
         0]["productPrice"]["current"]["text"]
 
+    domain_name = get_domain_name(url)
+
     wanted_prod_data = {
         "product_name": product_data["name"],
         "image_URL": product_data["image"],
-        "price": price
+        "price": price,
+        "product_url": url,
+        "website_name": domain_name
     }
 
     return wanted_prod_data

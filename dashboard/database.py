034f3a6c0b365fe5825fb0b5161a0123b98bc128
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2024-01-06 01:48:45 +0000

    Refactored

--- dashboard/database.py
+++ dashboard/database.py
@@ -3,22 +3,31 @@ Establishes a connection to the database.
 """
 from os import environ
 
-from boto3 import client
 import pandas as pd
 from pandas import DataFrame
 from dotenv import load_dotenv
-from psycopg2 import connect, extras
+from psycopg2 import connect
 from psycopg2.extensions import connection
+from psycopg2.extras import RealDictCursor
 
-SELECT_ALL_QUERY = """SELECT prices.price_id, prices.updated_at, prices.price, products.product_id, products.product_name, products.product_url, products.website_name, products.image_url, products.product_availability, users.user_id, users.email, users.first_name, users.last_name, subscriptions.subscription_id
+SELECT_ALL_QUERY = """SELECT prices.price_id, prices.updated_at,
+                prices.price, products.product_id, products.product_name,
+                products.product_url, products.website_name, products.image_url,
+                products.product_availability, users.user_id, users.email,
+                users.first_name, users.last_name, subscriptions.subscription_id
                 FROM prices
                 JOIN products ON products.product_id = prices.product_id
                 JOIN subscriptions ON subscriptions.product_id = products.product_id
                 JOIN users ON users.user_id = subscriptions.user_id
                 ORDER BY product_id, updated_at DESC;"""
 
-COLUMNS = {"price_id": "Price ID", "updated_at": "Updated At", "price": "Price", "product_id": "Product ID", "product_name": "Product Name", "product_url": "Product URL", "image_url": "Image URL",
-           "product_availability": "Product Availability", "website_name": "Website Name", "user_id": "User ID", "first_name": "User FirstName", "last_name": "User LastName", "email": "User Email", "subscription_id": "Subscription ID"}
+COLUMNS = {"price_id": "Price ID", "updated_at": "Updated At",
+           "price": "Price", "product_id": "Product ID",
+           "product_name": "Product Name", "product_url": "Product URL",
+           "image_url": "Image URL", "product_availability": "Product Availability",
+           "website_name": "Website Name", "user_id": "User ID",
+           "first_name": "User FirstName", "last_name": "User LastName",
+           "email": "User Email", "subscription_id": "Subscription ID"}
 
 
 def get_database_connection() -> connection:
@@ -37,16 +46,43 @@ def get_database_connection() -> connection:
         return error
 
 
-def load_all_database_info(db_conn: connection) -> pd.DataFrame:
+def load_all_database_info(db_conn: connection) -> DataFrame:
     """Extract all data from the database."""
 
-    with db_conn.cursor(cursor_factory=extras.RealDictCursor) as cur:
+    with db_conn.cursor(cursor_factory=RealDictCursor) as cur:
 
         cur.execute(SELECT_ALL_QUERY)
 
         result = cur.fetchall()
 
         return pd.DataFrame(result).rename(columns=COLUMNS)
+
+
+def get_user_info(conn: connection) -> list[dict]:
+    """
+    Gets all of the information from the users table in the RDS,
+    adds a password and account type to each user 
+    and returns a list of all users.
+    """
+    all_users = [
+        {"user_id": 0,
+         "email": "admin@saletracker.co.uk",
+         "password": "adminPassword",
+         "type": "admin",
+         "first_name": "Admin",
+         "last_name": "Admin"},
+    ]
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute("SELECT * FROM users;")
+    users = cur.fetchall()
+
+    for user in users:
+        user["password"] = 'userPassword'
+        user["type"] = 'user'
+        all_users.append(user)
+
+    return all_users
 
 
 if __name__ == "__main__":

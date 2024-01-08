1c1db1d4738742d667e1d65a2e718620bbe47655
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2024-01-08 18:09:14 +0000

    added graphs

--- dashboard/visualisations.py
+++ dashboard/visualisations.py
@@ -1,16 +1,13 @@
 """
 Functions to visualise data on streamlit.
 """
-from os import environ
-
 import altair as alt
-from dotenv import load_dotenv
 import pandas as pd
 from pandas import DataFrame
-import streamlit as st
 
-
-from database import get_database_connection, get_user_info
+NUM_COLUMNS_LEGEND = 3
+MAX_WIDTH = 600
+MAX_HEIGHT = 400
 
 
 def get_latest_price_readings(latest_data: DataFrame) -> alt.vegalite.v5.api.Chart:
@@ -21,7 +18,6 @@ def get_latest_price_readings(latest_data: DataFrame) -> alt.vegalite.v5.api.Cha
     latest_data["Price (£)"] = latest_data["Price"].astype(float)
 
     latest_data = latest_data[["Product Name", "Price (£)"]]
-    print(latest_data)
 
     latest_price_readings = alt.Chart(latest_data).mark_bar().encode(
         y=alt.Y('Product Name:N'),
@@ -30,6 +26,51 @@ def get_latest_price_readings(latest_data: DataFrame) -> alt.vegalite.v5.api.Cha
         color=alt.Color('Product Name:N', legend=None).scale(scheme='blues')
     ).properties(
         title='Latest Price of Products',
-        width=600
+        width=MAX_WIDTH
     )
     return latest_price_readings
+
+
+def get_popularity_of_products(df: DataFrame) -> alt.vegalite.v5.api.Chart:
+    """
+    Displays the popularity (number of subscriptions each product has) over time.
+    """
+    grouped = df.groupby('Product Name')
+
+    product_popularity = grouped['Subscription ID'].nunique()
+    product_popularity = product_popularity.reset_index()
+    product_popularity.columns = ['Product Name', 'Popularity']
+
+    popularity = alt.Chart(product_popularity).mark_bar().encode(
+        y=alt.Y('Product Name:N'),
+        x=alt.X('Popularity:Q'),
+        tooltip=['Product Name:N', 'Popularity:Q'],
+        color=alt.Color('Product Name:N', legend=None).scale(scheme='blues')
+    ).properties(
+        title='Product Popularity',
+        width=MAX_WIDTH
+    )
+    return popularity
+
+
+def get_price_of_products_over_time(df: DataFrame) -> alt.vegalite.v5.api.Chart:
+    """
+    Displays the price of all products over time in a line graph.
+    """
+    df['Updated At'] = pd.to_datetime(df['Updated At'])
+
+    df['Price'] = df['Price'].astype(float)
+
+    line_chart = alt.Chart(df).mark_line().encode(
+        x=alt.X('Updated At:T', axis=alt.Axis(title='Time')),
+        y=alt.Y('Price:Q', axis=alt.Axis(title='Price')),
+        color=alt.Color('Product Name:N', legend=alt.Legend(
+            orient='bottom', columns=NUM_COLUMNS_LEGEND)),
+        tooltip=['Product Name:N', 'Price:Q', 'Updated At:T']
+    ).properties(
+        title='Price Over Time by Product',
+        width=MAX_WIDTH,
+        height=MAX_HEIGHT
+    )
+
+    return line_chart

43a3771eb17995227377432ee42d76d302242217
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2024-01-08 13:12:13 +0000

    Added more side buttons

--- dashboard/rendering.py
+++ dashboard/rendering.py
@@ -1,7 +1,10 @@
+import webbrowser
 
 import pandas as pd
 from pandas import DataFrame
 import streamlit as st
+
+from visualisations import get_latest_price_readings
 
 
 def render_login_page() -> tuple:
@@ -19,6 +22,28 @@ def render_login_page() -> tuple:
     return email, password
 
 
+def get_most_recent_price(most_recent: DataFrame):
+    """
+    Returns the most recent price information.
+    """
+    sorted_product_df = most_recent.sort_values(
+        by='Updated At', ascending=False)
+    most_recent_price = sorted_product_df.iloc[0]
+    return most_recent_price
+
+
+def get_price_info(most_recent: DataFrame, sort_value):
+    """
+    Displays the price information for the most recent price for a particular product. 
+    If sort_value is False the highest price is returned. 
+    If the sort_value is True the lowest price is returned.
+    """
+    sorted_product_df = most_recent.sort_values(
+        by='Price', ascending=sort_value)
+    price = sorted_product_df.iloc[0]
+    return price
+
+
 def render_product_image_viewer(df: DataFrame) -> None:
     """
     Creates a sidebar element that shows the image of the product
@@ -29,13 +54,39 @@ def render_product_image_viewer(df: DataFrame) -> None:
         'Select a Product', df['Product Name'].unique())
     filtered_df = df[df['Product Name'] == selected_product_name]
     if not filtered_df.empty:
+
         image_url = filtered_df['Image URL'].iloc[0]
         st.sidebar.image(image_url)
+
+        most_recent_price = get_most_recent_price(filtered_df)
+        st.sidebar.write(
+            f'Current price: £{round(most_recent_price["Price"], 2)}')
+
+        highest_price = get_price_info(filtered_df, False)
+        st.sidebar.write(
+            f'Highest Price: £{round(highest_price["Price"], 2)}')
+
+        lowest_price = get_price_info(filtered_df, True)
+        st.sidebar.write(f'Lowest Price: £{round(lowest_price["Price"], 2)}')
+
+        if most_recent_price["Product Availability"] == True:
+            st.sidebar.write(
+                f'Availability: In Stock')
+        else:
+            st.sidebar.write(
+                f'Availability: Out of Stock')
+
+        if st.sidebar.button("Go to Product"):
+            webbrowser.open_new_tab(most_recent_price["Product URL"])
     else:
         st.sidebar.write("No image available for the selected product.")
 
 
 def display_admin_main_body(df: DataFrame) -> None:
+    sorted_df = df.sort_values(
+        by=['Product ID', 'Updated At'], ascending=[True, False])
+    most_recent_prices = sorted_df.groupby('Product ID').first()
+    print(most_recent_prices)
     head_cols = st.columns(3)
 
     with head_cols[0]:
@@ -47,6 +98,12 @@ def display_admin_main_body(df: DataFrame) -> None:
     with head_cols[2]:
         st.metric("Total No. of Subscriptions",
                   df["Subscription ID"].nunique())
+
+    body_cols = st.columns(2)
+
+    with body_cols[0]:
+        st.altair_chart(get_latest_price_readings(
+            most_recent_prices), use_container_width=True)
 
 
 def render_admin_dashboard(df: DataFrame, users: list[dict]) -> None:
@@ -74,6 +131,18 @@ def display_user_specific_data(df: DataFrame) -> None:
         by=['Product ID', 'Updated At'], ascending=[True, False])
     most_recent_prices = sorted_df.groupby('Product ID').first()
     st.write(f"Dashboard for User ID {st.session_state['user_id']}")
+    print(most_recent_prices)
+
+    body_cols = st.columns(2)
+
+    with body_cols[0]:
+        st.write("Latest price readings")
+        st.altair_chart(get_latest_price_readings(
+            most_recent_prices), use_container_width=True)
+
+    with body_cols[1]:
+        st.write("Product Popularity")
+
     st.table(most_recent_prices)
 
 
